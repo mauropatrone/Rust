99a1aa58053e5d75faca0eb09924c8fe278c9793
mauropatrone <patrone.mauro@gmail.com>
2022-12-08 12:26:12 -0300

    Delete target directory of hello project

--- projects/hello/target/debug/deps/hello-3b6fcb9eb129c0f3.d
+++
@@ -1,5 +0,0 @@
-/home/mauro/_Repositories/Rust/projects/hello/target/debug/deps/hello-3b6fcb9eb129c0f3: src/main.rs
-
-/home/mauro/_Repositories/Rust/projects/hello/target/debug/deps/hello-3b6fcb9eb129c0f3.d: src/main.rs
-
-src/main.rs:
--- projects/hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/home/mauro/_Repositories/Rust/projects/hello/target/debug/hello: /home/mauro/_Repositories/Rust/projects/hello/src/main.rs
